27c0ef1305b8461400313e5abb190c7036be440f
Jaroslav Němec <j.nemecc9@gmail.com>
2024-12-06 21:25:56 +0100

    Day Six

--- AOC2024/6/guard_pathfinder.py
+++ AOC2024/6/guard_pathfinder.py
@@ -8,62 +8,62 @@ class Parser:
     def parse(self):
         with open(self.input_file, 'r') as file:
             return [[a for a in i] for i in file.read().splitlines()]
+    
 
+def find_guard(maze):
+    for i in range(len(maze)):
+        for j in range(len(maze[i])):
+            if maze[i][j] == '^' or maze[i][j] == 'v' or maze[i][j] == '<' or maze[i][j] == '>':
+                return (i, j)
+
+
+def check_direction(guard, direction):
+    if direction == '^':
+        return (guard[0] - 1, guard[1])
+    elif direction == 'v':
+        return (guard[0] + 1, guard[1])
+    elif direction == '<':
+        return (guard[0], guard[1] - 1)
+    elif direction == '>':
+        return (guard[0], guard[1] + 1)
+
+def check_maze(maze, guard):
+    visited = []
+    collided_wals = []
+    guard_direction = maze[guard[0]][guard[1]]
+    visited.append(guard)
+
+    while True:
+        next_field = check_direction(guard, guard_direction)
+        if next_field[0] < 0 or next_field[0] > len(maze)-1 or next_field[1] < 0 or next_field[1] > len(maze[0])-1:
+            break
+        if maze[next_field[0]][next_field[1]] == '#':
+            if (next_field, guard_direction) in collided_wals:
+                return None
+            collided_wals.append((next_field, guard_direction))
+            if guard_direction == '^':
+                guard_direction = '>'
+            elif guard_direction == '>':
+                guard_direction = 'v'
+            elif guard_direction == 'v':
+                guard_direction = '<'
+            elif guard_direction == '<':
+                guard_direction = '^'
+        else:
+            guard = next_field
+            visited.append(guard)
+
+
+    return list(set(visited))
 if __name__ == '__main__':
     possible_infinite_loops = 0
     parser = Parser('input.txt')
     maze = parser.parse()
-    
-    
-
-    def find_guard(maze):
-        for i in range(len(maze)):
-            for j in range(len(maze[i])):
-                if maze[i][j] == '^' or maze[i][j] == 'v' or maze[i][j] == '<' or maze[i][j] == '>':
-                    return (i, j)
-
     guard = find_guard(maze)
 
-    def check_direction(guard, direction):
-        if direction == '^':
-            return (guard[0] - 1, guard[1])
-        elif direction == 'v':
-            return (guard[0] + 1, guard[1])
-        elif direction == '<':
-            return (guard[0], guard[1] - 1)
-        elif direction == '>':
-            return (guard[0], guard[1] + 1)
-
-    def check_maze(maze, guard=guard):
-        visited = []
-        collided_wals = []
-        guard_direction = maze[guard[0]][guard[1]]
-        visited.append(guard)
-
-        while True:
-            next_field = check_direction(guard, guard_direction)
-            if next_field[0] < 0 or next_field[0] > len(maze)-1 or next_field[1] < 0 or next_field[1] > len(maze[0])-1:
-                break
-            if maze[next_field[0]][next_field[1]] == '#':
-                if (next_field, guard_direction) in collided_wals:
-                    return None
-                collided_wals.append((next_field, guard_direction))
-                if guard_direction == '^':
-                    guard_direction = '>'
-                elif guard_direction == '>':
-                    guard_direction = 'v'
-                elif guard_direction == 'v':
-                    guard_direction = '<'
-                elif guard_direction == '<':
-                    guard_direction = '^'
-            else:
-                guard = next_field
-                visited.append(guard)
 
 
-        return list(set(visited))
-
-    all_visited_places = check_maze(maze)
+    all_visited_places = check_maze(maze, guard)
     print(len(all_visited_places))
 
     for place in all_visited_places:
@@ -71,7 +71,7 @@ if __name__ == '__main__':
         if place == guard:
             continue
         maze[place[0]][place[1]] = '#'
-        if check_maze(maze) == None:
+        if check_maze(maze, guard) == None:
             possible_infinite_loops += 1
     
 
--- AOC2024/6/test_guard_pathfinder.py
+++ AOC2024/6/test_guard_pathfinder.py
@@ -2,5 +2,22 @@ import unittest
 import guard_pathfinder
 
 class TestGuardPathfinder(unittest.TestCase):
-    def setUp(self:):
-        pass
+    def setUp(self):
+        self.parser = guard_pathfinder.Parser('test_input.txt')
+        self.maze = self.parser.parse()
+        self.guard = guard_pathfinder.find_guard(self.maze)
+
+    def test_find_guard(self):
+        self.assertEqual(self.guard, (6, 4))
+    
+    def test_check_direction(self):
+        self.assertEqual(guard_pathfinder.check_direction(self.guard, '^'), (5, 4))
+        self.assertEqual(guard_pathfinder.check_direction(self.guard, 'v'), (7, 4))
+        self.assertEqual(guard_pathfinder.check_direction(self.guard, '<'), (6, 3))
+        self.assertEqual(guard_pathfinder.check_direction(self.guard, '>'), (6, 5))
+    
+    def test_check_maze(self):
+        self.assertEqual(len(guard_pathfinder.check_maze(self.maze, self.guard)), 41)
+
+if __name__ == '__main__':
+    unittest.main()
